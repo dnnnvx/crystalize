9c0b9bc4291aacd9595d1334467e4f417f1adda3
dnnnvx <dotmarcodestefani@gmail.com>
2022-06-07 20:39:54 +0200

    final task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState('idle')
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    setState('loading')
+    window.fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum', {}).then(res => {
+      if (res.ok) {
+        res.json().then(d => setData(d?.length ? d[0] : undefined))
+        setState('idle')
+      } else {
+        setState('error')
+      }
+    }).catch(e => {
+      console.log(e)
+      setState('error')
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state === 'loading' ? (
+        <p>fetching data...</p>
+      ) : state === 'error' ? (
+        <p>something went wrong :(</p>
+      ) : data ? (
+        <div className="infos">
+          <img alt="symbol" src={data.image} className="logo" />
+          <p className="title">{data.name}</p>
+          <p className="price">current price:<br /><span>${data.current_price}</span></p>
+          <p className="date">last update: {new Date(data.last_updated).toLocaleString()}</p>
+        </div>
+      ) : (
+        <p>...</p>
+      )}
     </div>
   );
 }
--- src/tasks.js
+++ src/tasks.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 export const aay = [1, 2, 3]
 
 export const Counter = () => {
-  const [counter, setCounter] = useState()
+  const [counter, setCounter] = useState(0)
   return (
     <p>
       {counter}
@@ -16,7 +16,7 @@ export const bubbleSort = (a = []) => {
   for (let i = 1; i < a.length; i++) {
     for (let j = 0; j < a.length - i; j++) {
       if (a[j] > a[j + 1]) {
-        var tmp = a[j];
+        const tmp = a[j];
         a[j] = a[j+1];
         a[j + 1] = tmp;
       }
